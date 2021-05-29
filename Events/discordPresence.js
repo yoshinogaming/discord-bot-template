67cda95209fd103205b36b0f39048ffdc4209979
mangadi3859 <65892564+mangadi3859@users.noreply.github.com>
2021-05-30 00:15:19 +0800

    Typo meh

--- Events/discordPresence.js
+++ Events/discordPresence.js
@@ -1,5 +1,5 @@
 const { prefix } = require("../data/config.json");
-const intervalPerPercense = 60000; //Cooldown between any presence loop in ms.
+const intervalPerPrecense = 60000; //Cooldown between any presence loop in ms.
 
 module.exports = async (bot) => {
     //List of presence to be diplay on bot profile.
@@ -12,5 +12,5 @@ module.exports = async (bot) => {
         if (index > presences.length - 1) index = 0;
         bot.user.setActivity(presences[index].activity, { type: presences[index].type });
         index++;
-    }, intervalPerPercense);
+    }, intervalPerPrecense);
 };
